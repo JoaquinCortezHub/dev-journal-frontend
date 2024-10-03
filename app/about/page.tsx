7c249b24b4bde82db49a3f8cc89fe13333fe1424
Joaquin <joaquinlucascortez@gmail.com>
2024-10-03 09:18:41 -0300

    Set the about page with the desired layout, soon to complete, fix the style of links in the blog

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,76 +1,92 @@
-import { buttonVariants } from "@/components/ui/button";
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
-import Twitter from "@geist-ui/icons/twitter";
-import Github from "@geist-ui/icons/github";
-import Mail from "@geist-ui/icons/mail";
+import { buttonVariants } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import Twitter from '@geist-ui/icons/twitter'
+import Github from '@geist-ui/icons/github'
+import Mail from '@geist-ui/icons/mail'
 
 const AboutPage = () => {
-	return (
-		<div className="px-4 sm:px-8 md:px-12">
-			<div className="container mx-auto h-[250px] rounded-2xl">
-				<Image
-					src="/cover-pic.jpg"
-					alt="blog banner"
-					width={2400}
-					height={2400}
-					className="w-full h-full object-cover rounded-2xl"
-				/>
-			</div>
-			<div className="container mx-auto">
-				<div className="flex justify-center w-[56] h-[56] -mt-16">
-					<Image
-						src="/profile.jpg"
-						alt="profile"
-						width={100}
-						height={100}
-						className="rounded-full h-28 object-cover"
-					/>
-				</div>
-			</div>
-			<div className="text-center mt-2">
-				<h1 className="text-5xl font-bold text-slate-800 dark:text-stone-200 leading-tight">
-					Joaquín Cortez
-				</h1>
-				<h3 className="text-lg text-slate-500 font-medium">
-					Junior Fullstack Developer | UX Designer
-				</h3>
-				<div className="flex items-center justify-center mt-4 gap-4">
-					<Link
-						href={"https://x.com/JoacoLCortez"}
-						className={buttonVariants({ variant: "outline", size: "icon" })}
-					>
-						<Twitter color="gray" />
-					</Link>
-					<Link
-						href="https://mail.google.com/mail/?view=cm&fs=1&to=joaquinlucascortez@gmail.com"
-						passHref
-						legacyBehavior
-					>
-						<a 
-							target="_blank"
-							className={buttonVariants({ variant: 'outline', size: 'icon' })}
-						>
-							<Mail color="gray" />
-						</a>
-					</Link>
-					<Link
-						href={"https://github.com/JoaquinCortezHub"}
-						className={buttonVariants({ variant: "outline", size: "icon" })}
-					>
-						<Github color="gray" />
-					</Link>
-				</div>
-			</div>
-			<footer>
-				<a 
-					href="https://unsplash.com/@ricvath"
-				>
-				</a>
-			</footer>
-		</div>
-	);
-};
+    return (
+        <div className="px-4 sm:px-8 md:px-12 flex flex-col min-h-[100vh]">
+            <div className="container mx-auto h-[250px] rounded-2xl">
+                <Image
+                    src="/cover.jpg"
+                    alt="blog banner"
+                    width={2400}
+                    height={2400}
+                    className="h-full w-full rounded-2xl object-cover"
+                />
+            </div>
+            <div className="container mx-auto">
+                <div className="-mt-16 flex h-[56] w-[56] justify-center">
+                    <Image
+                        src="/profile.jpg"
+                        alt="profile"
+                        width={100}
+                        height={100}
+                        className="h-28 rounded-full object-cover"
+                    />
+                </div>
+            </div>
+            <div className="mt-2 text-center">
+                <h1 className="text-5xl font-bold leading-tight text-slate-800 dark:text-stone-200">
+                    Joaquín Cortez
+                </h1>
+                <h3 className="text-lg font-medium text-slate-500">
+                    Junior Fullstack Developer | UX Designer
+                </h3>
+                <div className="mt-4 flex items-center justify-center gap-4">
+                    <Link
+                        href={'https://x.com/JoacoLCortez'}
+                        className={buttonVariants({
+                            variant: 'outline',
+                            size: 'icon',
+                        })}
+                    >
+                        <Twitter color="gray" />
+                    </Link>
+                    <Link
+                        href="https://mail.google.com/mail/?view=cm&fs=1&to=joaquinlucascortez@gmail.com"
+                        passHref
+                        legacyBehavior
+                    >
+                        <a
+                            target="_blank"
+                            className={buttonVariants({
+                                variant: 'outline',
+                                size: 'icon',
+                            })}
+                        >
+                            <Mail color="gray" />
+                        </a>
+                    </Link>
+                    <Link
+                        href={'https://github.com/JoaquinCortezHub'}
+                        className={buttonVariants({
+                            variant: 'outline',
+                            size: 'icon',
+                        })}
+                    >
+                        <Github color="gray" />
+                    </Link>
+                </div>
+            </div>
+            <div className="container mx-auto mb-16 max-w-4xl px-4 sm:px-6 lg:px-8 mt-6">
+                <h2 className='text-2xl text-slate-800 dark:text-slate-200 text-center'>Coming soon...</h2>
+            </div>
+            <footer className='flex items-center justify-center mt-auto my-6'>
+                <a
+                    href="https://unsplash.com/@ricvath"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-sm text-gray-800 underline dark:text-gray-300"
+                >
+                    Cover image by @ricvath | Unsplashed
+                </a>
+            </footer>
+        </div>
+    )
+}
 
-export default AboutPage;
+export default AboutPage
--- components/PortableTextComponents.tsx
+++ components/PortableTextComponents.tsx
@@ -1,82 +1,104 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import Image from "next/image";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
-import { SanityImageSource } from "@sanity/image-url/lib/types/types";
-import imageUrlBuilder from "@sanity/image-url";
-import { client } from "@/sanity/client";
-import { PortableTextComponents } from "next-sanity";
+import Image from 'next/image'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { SanityImageSource } from '@sanity/image-url/lib/types/types'
+import imageUrlBuilder from '@sanity/image-url'
+import { client } from '@/sanity/client'
+import { PortableTextComponents } from 'next-sanity'
 
-const { projectId, dataset } = client.config();
+const { projectId, dataset } = client.config()
 const urlFor = (source: SanityImageSource) =>
-	projectId && dataset
-		? imageUrlBuilder({ projectId, dataset }).image(source)
-		: null;
+    projectId && dataset
+        ? imageUrlBuilder({ projectId, dataset }).image(source)
+        : null
 
 const components: PortableTextComponents = {
-	types: {
-		image: ({ value }: any) => {
-			const imageUrl = urlFor(value.asset)?.url();
-			return (
-				<Image
-					src={imageUrl!}
-					alt="Blog Image"
-					className="w-full h-auto object-cover my-4"
-					width={800}
-					height={450}
-				/>
-			);
-		},
-		code: ({ value }: any) => {
-			const { code, language } = value;
-			return (
-				<SyntaxHighlighter language={language || "javascript"} style={oneDark}>
-					{code}
-				</SyntaxHighlighter>
-			);
-		},
-	},
-	list: {
-		// Custom styles for unordered lists
-		bullet: ({ children }: any) => (
-			<ul className="list-disc ml-5 space-y-2">{children}</ul>
-		),
-		// Custom styles for ordered lists
-		number: ({ children }: any) => (
-			<ol className="list-decimal ml-5 space-y-2">{children}</ol>
-		),
-	},
-	listItem: {
-		// Custom styles for each list item
-		bullet: ({ children }: any) => (
-			<li className="text-slate-700 dark:text-stone-300">{children}</li>
-		),
-		number: ({ children }: any) => (
-			<li className="text-slate-700 dark:text-stone-300">{children}</li>
-		),
-	},
-	block: {
-		blockquote: ({ children }: any) => (
-			<blockquote className="border-l-4 border-gray-300 pl-4 italic text-gray-600 my-4">
-				{children}
-			</blockquote>
-		),
-		h1: ({ children }: any) => (
-			<h1 className="text-4xl font-semibold my-4">{children}</h1>
-		),
-		h2: ({ children }: any) => (
-			<h2 className="text-3xl font-semibold my-4">{children}</h2>
-		),
-		normal: ({ children }: any) => (
-			<p className="text-lg leading-relaxed my-4">{children}</p>
-		),
-		bullet: ({ children }: any) => (
-			<ul className="list-disc list-inside my-4">{children}</ul>
-		),
-		number: ({ children }: any) => (
-			<ol className="list-decimal list-inside my-4">{children}</ol>
-		),
-	},
-};
+    types: {
+        image: ({ value }: any) => {
+            const imageUrl = urlFor(value.asset)?.url()
+            return (
+                <Image
+                    src={imageUrl!}
+                    alt="Blog Image"
+                    className="my-4 h-auto w-full object-cover"
+                    width={800}
+                    height={450}
+                />
+            )
+        },
+        code: ({ value }: any) => {
+            const { code, language } = value
+            return (
+                <SyntaxHighlighter
+                    language={language || 'javascript'}
+                    style={oneDark}
+                >
+                    {code}
+                </SyntaxHighlighter>
+            )
+        },
+    },
+    marks: {
+        link: ({ children, value }: any) => {
+            const target = value?.href?.startsWith('http')
+                ? '_blank'
+                : undefined
+            return (
+                <a
+                    href={value.href}
+                    target={target}
+                    rel={
+                        target === '_blank' ? 'noopener noreferrer' : undefined
+                    }
+                    className="font-medium text-slate-800 dark:text-slate-300 underline"
+                >
+                    {children}
+                </a>
+            )
+        },
+    },
+    list: {
+        // Custom styles for unordered lists
+        bullet: ({ children }: any) => (
+            <ul className="ml-5 list-disc space-y-2">{children}</ul>
+        ),
+        // Custom styles for ordered lists
+        number: ({ children }: any) => (
+            <ol className="ml-5 list-decimal space-y-2">{children}</ol>
+        ),
+    },
+    listItem: {
+        // Custom styles for each list item
+        bullet: ({ children }: any) => (
+            <li className="text-slate-700 dark:text-stone-300">{children}</li>
+        ),
+        number: ({ children }: any) => (
+            <li className="text-slate-700 dark:text-stone-300">{children}</li>
+        ),
+    },
+    block: {
+        blockquote: ({ children }: any) => (
+            <blockquote className="my-4 border-l-4 border-gray-300 pl-4 italic text-gray-600">
+                {children}
+            </blockquote>
+        ),
+        h1: ({ children }: any) => (
+            <h1 className="my-4 text-4xl font-semibold">{children}</h1>
+        ),
+        h2: ({ children }: any) => (
+            <h2 className="my-4 text-3xl font-semibold">{children}</h2>
+        ),
+        normal: ({ children }: any) => (
+            <p className="my-4 text-lg leading-relaxed">{children}</p>
+        ),
+        bullet: ({ children }: any) => (
+            <ul className="my-4 list-inside list-disc">{children}</ul>
+        ),
+        number: ({ children }: any) => (
+            <ol className="my-4 list-inside list-decimal">{children}</ol>
+        ),
+    },
+}
 
-export default components;
+export default components
